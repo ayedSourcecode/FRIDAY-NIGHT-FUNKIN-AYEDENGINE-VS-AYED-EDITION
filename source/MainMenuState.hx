fd2b59fa2b295394d3ab39e1f0d2e8a95a38fd2a
ayedFNFmakerSource code <123101780+ayedSourcecode@users.noreply.github.com>
2023-02-15 16:03:40 +0100

    I AM ALL TIME I MISS ONE

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -25,7 +25,8 @@ using StringTools;
 
 class MainMenuState extends MusicBeatState
 {
-	public static var psychEngineVersion:String = '0.6.3'; //This is also used for Discord RPC
+	public static var AyedVersion:String = '1.5.0';
+	public static var AyedEngineVersion:String = '1.5.0'; //This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
