91e6f9e26b8503a11acfd9001804fe4ce2651525
ayedFNFmakerSource code <123101780+ayedSourcecode@users.noreply.github.com>
2023-02-17 08:05:02 +0100

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -107,7 +107,7 @@ class FreeplayState extends MusicBeatState
 		add(bg);
 		bg.screenCenter();
 
-		logoBl = new FlxSprite(-100, 0);
+		logoBl = new FlxSprite(150, 0);
 		logoBl.frames = Paths.getSparrowAtlas('logoBumpin');
 		
 		logoBl.antialiasing = ClientPrefs.globalAntialiasing;
