1da0c80e5c1d5539ae86a3e1b4b298b3ef4d8850
ayedFNFmakerSource code <123101780+ayedSourcecode@users.noreply.github.com>
2023-02-17 05:57:48 +0100

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -149,7 +149,7 @@ class FreeplayState extends MusicBeatState
 		scoreText = new FlxText(FlxG.width * 0.7, 5, 0, "", 32);
 		scoreText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, RIGHT);
 
-		scoreBG = new FlxSprite(scoreText.x - 6, 0).makeGraphic(1, 66, 0xFF000000);
+		scoreBG = new FlxSprite(scoreText.x - 6, 0).makeGraphic(1, 66, 0x0000FF);
 		scoreBG.alpha = 0.6;
 		add(scoreBG);
 
