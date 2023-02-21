b00ad86e08c10e409c3a1ede38a11f6996c7998b
ayedFNFmakerSource code <123101780+ayedSourcecode@users.noreply.github.com>
2023-02-21 12:24:44 +0100

    Update TitleERROR.hx
    Signed-off-by: ayedFNFmakerSource code <123101780+ayedSourcecode@users.noreply.github.com>

--- source/TitleERROR.hx
+++ source/TitleERROR.hx
@@ -183,33 +183,7 @@ class TitleERROR extends MusicBeatState {
 	var titleText:FlxSprite;
 	var swagShader:ColorSwap = null;
 
-	function startIntro() {
-		if (!initialized) {
-			/*var diamond:FlxGraphic = FlxGraphic.fromClass(GraphicTransTileDiamond);
-				diamond.persist = true;
-				diamond.destroyOnNoUse = false;
-
-				FlxTransitionableState.defaultTransIn = new TransitionData(FADE, FlxColor.BLACK, 1, new FlxPoint(0, -1), {asset: diamond, width: 32, height: 32},
-					new FlxRect(-300, -300, FlxG.width * 1.8, FlxG.height * 1.8));
-				FlxTransitionableState.defaultTransOut = new TransitionData(FADE, FlxColor.BLACK, 0.7, new FlxPoint(0, 1),
-					{asset: diamond, width: 32, height: 32}, new FlxRect(-300, -300, FlxG.width * 1.8, FlxG.height * 1.8));
-
-				transIn = FlxTransitionableState.defaultTransIn;
-				transOut = FlxTransitionableState.defaultTransOut; */
-
-			// HAD TO MODIFY SOME BACKEND SHIT
-			// IF THIS PR IS HERE IF ITS ACCEPTED UR GOOD TO GO
-			// https://github.com/HaxeFlixel/flixel-addons/pull/348
-
-			// var music:FlxSound = new FlxSound();
-			// music.loadStream(Paths.music('freakyMenu'));
-			// FlxG.sound.list.add(music);
-			// music.play();
-
-			if (FlxG.sound.music == null) {
-				FlxG.sound.playMusic(Paths.music('ERRORMENU'), 0);
-			}
-		}
+	function startIntro()
 
 		var bg:FlxSprite = new FlxSprite();
 		bg.loadGraphic(Paths.image('ERRRORBG'));
