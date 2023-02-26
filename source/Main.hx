211b637b31a496d2baa601ad6bc025fc585a352d
ayedFNFmakerSource code <123101780+ayedSourcecode@users.noreply.github.com>
2023-02-26 04:36:15 +0100

    Update Main.hx
    Signed-off-by: ayedFNFmakerSource code <123101780+ayedSourcecode@users.noreply.github.com>

--- source/Main.hx
+++ source/Main.hx
@@ -33,7 +33,7 @@ class Main extends Sprite
 	var game = {
 		width: 1280, // WINDOW width
 		height: 720, // WINDOW height
-		initialState: TitleState, // initial game state
+		initialState: Cache, // initial game state
 		zoom: -1.0, // game state bounds
 		framerate: 60, // default framerate
 		skipSplash: true, // if the default flixel splash screen should be skipped
